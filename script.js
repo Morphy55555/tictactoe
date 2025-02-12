9b72cb36ba0fff57aaec384f6ce7e3f3d7d09b53
Morphy55555 <learningthings1996@gmail.com>
2025-02-12 14:54:48 +0700

    Added objects for game controller and game logic, began basic html/css

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+container = document.querySelector('.container');
+
+
+
 //Factory function, each empty array slot is a board slot
 let gameBoard = (() => {
     let board = [];
     for (let i = 0; i < 9; i++) {
-        board.push("")
+        board.push("");
+        createDiv = document.createElement('div');
+        createDiv.classList.add(`tiles`, `div${i}`);
+        
+        container.appendChild(createDiv);
+        
     }
     return { board };
 })();
 console.log(gameBoard.board);
+
 
 const players = {
     player1: 'x',
@@ -19,7 +29,6 @@ const gameController = {
     o: 0,
     index: 0,
     counter: 0,
-    
 
     //adds player symbol to the board, alternating between player with a counter
     playerMove: (index) => {
@@ -27,56 +36,64 @@ const gameController = {
         if (gameController.counter % 2 === 0 &&
             //and the array index is empty
             gameBoard.board[index] === '') {
-                //add player 1 to the board
+            //add player 1 to the board
             gameBoard.board[index] = players.player1;
             gameController.counter++;
-            
+
             //else if its odd and empty, add player 2
-        } else if (gameBoard.board[index] === ''){
+        } else if (gameBoard.board[index] === '') {
             gameBoard.board[index] = players.player2;
             gameController.counter++;
-            
-        }
-        
-        console.log(gameBoard.board);
-        checkWin();
-      }
-    };
 
-    
-    const checkWin = () => {
-        // Check rows
+        } else return;
+
+        console.log(gameBoard.board);
+        gameLogic.checkWin();
+    },
+
+    resetBoard: ()=> {
+        gameBoard.board = [];
+        for (i = 0; i < 9; i ++) {
+            gameBoard.board.push('')
+        }
+    }
+
+};
+
+const gameLogic = {
+    checkWin: () => {
+        // Check rows for matching x / o
         if (
             (gameBoard.board[0] === gameBoard.board[1] && gameBoard.board[1] === gameBoard.board[2] && gameBoard.board[0] !== '') ||
             (gameBoard.board[3] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[5] && gameBoard.board[3] !== '') ||
             (gameBoard.board[6] === gameBoard.board[7] && gameBoard.board[7] === gameBoard.board[8] && gameBoard.board[6] !== '')
         ) {
-            console.log("We got a winner!");
+            console.log("We got a wiener!");
+            gameController.resetBoard();
             return true;
         }
-    
+
         // Check columns
         if (
             (gameBoard.board[0] === gameBoard.board[3] && gameBoard.board[3] === gameBoard.board[6] && gameBoard.board[0] !== '') ||
             (gameBoard.board[1] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[7] && gameBoard.board[1] !== '') ||
             (gameBoard.board[2] === gameBoard.board[5] && gameBoard.board[5] === gameBoard.board[8] && gameBoard.board[2] !== '')
         ) {
-            console.log("We got a winner!");
+            console.log("We got a wiener!");
+            gameController.resetBoard();
             return true;
         }
-    
+
         // Check diagonals
         if (
             (gameBoard.board[0] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[8] && gameBoard.board[0] !== '') ||
             (gameBoard.board[2] === gameBoard.board[4] && gameBoard.board[4] === gameBoard.board[6] && gameBoard.board[2] !== '')
         ) {
-            console.log("We got a winner!");
+            console.log("We got a wiener!");
+            gameController.resetBoard();
             return true;
         }
-    
-        return false; // No winner yet
-    };
-    
-
-
+        return false;
+    }
+}
 
